43a19a11147bf61dca446d28321b1a8fdcd8e8fd
hades <quentindevial19@gmail.com>
2018-01-31 11:29:59 +0100

    update module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Exercice1Component } from './exercice-1/exercice-1.component';
-import { ColorDirective } from './exercice-1/color.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Exercice1Component,
-    ColorDirective,
   ],
   imports: [
     BrowserModule
